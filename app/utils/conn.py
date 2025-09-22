e5b72fac145abdba078b46506517554ef8f643ba
naxg <28560949+NAXG@users.noreply.github.com>
2025-09-23 00:12:29 +0800

    优化 conn.py 文件，移除未使用的导入和冗余的内容处理逻辑，简化 HTTP 请求函数，提高代码的可读性和一致性。

--- app/utils/conn.py
+++ app/utils/conn.py
@@ -1,14 +1,9 @@
 import urllib3
-import time
 import requests
 from app.config import Config
 from pymongo import MongoClient
-from requests.exceptions import ReadTimeout
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-
-
-CONTENT_CHUNK_SIZE = 10 * 1024
 
 
 UA = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
@@ -22,34 +17,11 @@ proxies = {
 SET_PROXY = False
 
 
-# requests/models.py:824
-def patch_content(response, timeout=None):
-    """Content of the response, in bytes."""
-    start_at = time.time()
-    if response._content is False:
-        # Read the contents.
-        if response._content_consumed:
-            raise RuntimeError("The content for this response was already consumed")
-
-        if response.status_code == 0 or response.raw is None:
-            response._content = None
-        else:
-            body = b''
-            for part in response.iter_content(CONTENT_CHUNK_SIZE):
-                body += part
-                if timeout is not None and time.time() - start_at >= timeout:
-                    raise ReadTimeout(f"patch_content read http response timeout: {timeout}")
-            response._content = body
-    response._content_consumed = True
-    # don't need to release the connection; that's been handled by urllib3
-    # since we exhausted the data.
-    return response._content
-
-
 def http_req(url, method='get', **kwargs):
     kwargs.setdefault('verify', False)
     kwargs.setdefault('timeout', (10.1, 30.1))
     kwargs.setdefault('allow_redirects', False)
+    kwargs.setdefault('stream', False)
 
     headers = kwargs.get("headers", {})
     headers.setdefault("User-Agent", UA)
@@ -65,12 +37,9 @@ def http_req(url, method='get', **kwargs):
         kwargs["proxies"] = proxies
 
     conn = getattr(requests, method)(url, **kwargs)
-
-    timeout = kwargs.get("timeout")
-    if len(timeout) > 1 and timeout[1]:
-        timeout = timeout[1]
-
-    patch_content(conn, timeout)
+    if not kwargs.get('stream'):
+        # 触发内容加载，让 requests 自行处理缓存
+        _ = conn.content
 
     return conn
 
