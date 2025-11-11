2279d76837f22a5350bd1628e6638cacbeab0f42
NAXG <NAXG@users.noreply.github.com>
2025-11-11 15:57:27 +0800

    update dns

--- misc/setup-arl.sh
+++ misc/setup-arl.sh
@@ -3,9 +3,10 @@ set -e
 cd /opt/
 
 tee /etc/resolv.conf <<"EOF"
-nameserver 180.76.76.76
-nameserver 4.2.2.1
 nameserver 1.1.1.1
+nameserver 8.8.8.8
+nameserver 223.5.5.5
+nameserver 119.29.29.29
 EOF
 
 
